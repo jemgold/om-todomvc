2bdaab2e1a70860d2031e35ccb047675d57acf1b
Jon Gold <hello@designedbygold.com>
2014-06-18 18:34:42 +0100

    Use om-tools defcomponent macro

--- src/om_todo/core.cljs
+++ src/om_todo/core.cljs
@@ -15,78 +15,70 @@
              {:text "Cook dinner"}
              {:text "Watch England lose *again*"}]}))
 
-(defn main [app owner]
-  (reify
-    om/IRender
-    (render [this]
-      (dom/div {:id "todoapp"}
-               (om/build header app)
-               (dom/section {:id "main"}
-                            (om/build toggle-all (:todos app))
-                            (om/build todo-list (:todos app)))
-               (om/build footer (:todos app))))))
+(defcomponent main [app owner]
+  (render [_]
+          (dom/div
+            {:id "todoapp"}
+            (om/build header app)
+            (dom/section
+              {:id "main"}
+              (om/build toggle-all (:todos app))
+              (om/build todo-list (:todos app)))
+            (om/build footer (:todos app)))))
 
-(defn todo-list [todos _]
-  (reify
-    om/IRender
-    (render [_]
-      (dom/ul {:id "todo-list"}
-              (om/build-all todo-view todos)))))
+(defcomponent todo-list [todos owner]
+  (render [_]
+          (dom/ul {:id "todo-list"}
+                  (om/build-all todo-view todos))))
 
-(defn todo-view [todo owner]
-  (reify
-    om/IRender
-    (render [this]
-      (let [class (cond-> ""
-                    (:completed todo) (str "completed"))]
-        (dom/li {:class class}
-                (dom/input
-                  {:class "toggle" :type "checkbox"
-                   :checked (:completed todo)
-                   :on-click (fn [e] (om/transact! todo :completed #(not %)))})
-                (dom/label (:text todo))
-                (dom/button {:class "destroy"}))))))
+(defcomponent todo-view [todo owner]
+  (render [_]
+          (let [class (cond-> ""
+                        (:completed todo) (str "completed"))]
+            (dom/li
+              {:class class}
+              (dom/input
+                {:class "toggle" :type "checkbox"
+                 :checked (:completed todo)
+                 :on-click (fn [e] (om/transact! todo :completed #(not %)))})
+              (dom/label (:text todo))
+              (dom/button {:class "destroy"})))))
 
-(defn toggle-all [todos _]
-  (reify
-    om/IRender
-    (render [_]
-      (dom/div
-        (dom/input
-          {:id "toggle-all" :type "checkbox"})
-        (dom/label
-          {:for "toggle-all"}
-          "Mark all as completed") ))))
+(defcomponent toggle-all [todos owner]
+  (render [_]
+          (dom/div
+            (dom/input
+              {:id "toggle-all" :type "checkbox"})
+            (dom/label
+              {:for "toggle-all"}
+              "Mark all as completed") )))
 
-(defn header [app owner]
-  (reify
-    om/IRender
-    (render [_]
-      (dom/section {:id "header"}
-                   (dom/h1 (:title app))
-                   (om/build new-todo-view app)))))
+(defcomponent header [app owner]
+  (render [_]
+          (dom/section
+            {:id "header"}
+            (dom/h1 (:title app))
+            (om/build new-todo-view app))))
 
-(defn new-todo-view [app owner]
-  (reify
-    om/IRender
-    (render [_]
-      (dom/input {:id "new-todo"
-                  :placeholder "What needs to be done?"
-                  :auto-focus true
-                  :on-key-press #(add-todo % app owner)  }
-                 ))))
+(defcomponent new-todo-view [app owner]
+  (render [_]
+          (dom/input
+            {:id "new-todo"
+             :placeholder "What needs to be done?"
+             :auto-focus true
+             :on-key-press #(add-todo % app owner)})))
 
-(defn footer [todos _]
-  (reify
-    om/IRender
-    (render [_]
-      (dom/footer {:id "footer"}
-                  (om/build todos-count todos) ))))
+(defcomponent footer [todos owner]
+  (render [_]
+          (dom/footer
+            {:id "footer"}
+            (om/build todos-count todos) )))
 
 (defn todos-count [todos]
-  (dom/span {:id "todo-count"}
-            (dom/strong (remaining todos))
-            " items left" ) )
+  (dom/span
+    {:id "todo-count"}
+    (dom/strong (remaining todos))
+    " items left" ))
 
 (defn remaining [todos]
   (->> todos
